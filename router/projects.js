2e42d2696e9760865f1194f50abe2e4483aa476b
hyunju <d15123649@mydit.ie>
2016-08-08 00:03:12 +0000

    wjdfl

--- router/projects.js
+++ router/projects.js
@@ -2,51 +2,30 @@
 module.exports = function(app, Project) {
 //user schema test
 
-/*
-  //모든 프로젝트 보는것은 따로 버튼 만들어서?????
-  app.get('/findmyproject', function(req,res){
-    sess = req.session;
-    var name = sess.name;
-    //console.log(name);
-    Project.find({pm: name}, function(err, myprojects){
-      if(err) return res.status(500).send({error: 'db failure'});
-      res.redirect('/dashboard');  //출력 잘됨!!
-      //res.redirect('/dashboard').send(myprojects);
-    });
-    //res.redirect('/dashboard');
-  });//getmyproject
-*/
-
   app.get('/dashboard', function(req,res){
     sess = req.session;
-    if(!sess.name) {
+    var name = sess.name;
+    
+    if(!name) {
       //로그인 안된 상태에서 들어오면
       res.redirect('/');
-    };
-
-    var name = sess.name;
-
-    console.log("dashboard loaded--");
+    };    
 
     Project.find({pm: name}, function(err, myprojects){
       if(err) return res.status(500).send({error: 'db failure'});
 
-      console.log(myprojects);  //출력 잘됨!!
-      //res.redirect('/dashboard').send(myprojects);
+//      console.log(myprojects);  //출력 잘됨!!
 
-      //name = req.session.name;
-      //console.log("session test: " + name);
       res.render('dashboard', {
         title: "Dashboard",
         length: 5,
         page_name: 'dashboard', // navbar set active에서 쓸 것
-        name: sess.name,
+        name: name,
         myprojects: myprojects
       })
     });
   });
-
-
+  
     app.get('/projects', function(req,res) {
       Project.find(function(err, projects) {
         if(err) return res.status(500).send({error: 'db failure'});
@@ -54,10 +33,10 @@ module.exports = function(app, Project) {
       })
     })
 
-    app.get('/api/projects/:project_id', function(req,res){
+    app.get('/projects/:project_id', function(req,res){
       res.end();
     });
-    app.get('/api/projects/title/:title', function(req,res){
+    app.get('/projects/title/:title', function(req,res){
       res.end();
     });
 
@@ -133,7 +112,7 @@ module.exports = function(app, Project) {
       }
       res.redirect('/dashboard');
       });
-    });//create a project
+    });//create a project and then go to dashboard 
 
   app.put('/api/projects/:project_id', function(req,res){
     res.end();
