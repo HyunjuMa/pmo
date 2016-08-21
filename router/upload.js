dd153ab6f63b364359e4e9dc549654651d911ce2
hyunjuma <flipped71@gmail.com>
2016-08-21 16:53:11 +0900

    123

--- router/upload.js
+++ router/upload.js
@@ -8,18 +8,17 @@ var mkdirp = require('mkdirp');
 router.post('/', function(req, res, next) {
 
 	var form = new multiparty.Form();
-	form.on('error', function(err) {
-		console.log('Error parsing form: ' + err.stack);
-	});
 
+//	res.setTimeout(0);
 
-	// Close emitted after form parsed
-	// form.on('close', function() {
-	// 	console.log('Upload completed! on Close');
-	// 	// res.setHeader('text/plain');
-	// 	res.end('Received ' + count + ' files');
-	// });
+/*
+	res.setTimeout(480000, function(){ // 4 minute timeout adjust for larger uploads
+        console.log('Request has timed out.');
+            res.send(408);
+        });
+*/
 
+	// file upload handling
 	form.on('part',function(part){
 		var filename;
 		var size;
@@ -30,54 +29,41 @@ router.post('/', function(req, res, next) {
 			part.resume();
 		}
 
-		part.on('error', function(err) {
-			console.log('error' + err.stack);
+    console.log("Write Streaming file :"+filename);
 
-		});
+    // get field name & value
+    form.on('field', function(name,value){
+      //console.log('normal field / name = '+name+' , value = '+value);
+			//여기서 밸류는 task id
+      var dir = ('/tmp/'+value);
+      mkdirp(dir, function(err) {
+      });
 
-	form.on('field', function(name, value){
-		//console.log('normal field / name = '+name+' , value = '+value);
-		//여기서 밸류는 task id
-		var dir = ('/tmp/'+value);
-		mkdirp(dir, function(err) {
-		});
+      var writeStream = fs.createWriteStream('/tmp/'+value+'/'+filename);
+      part.pipe(writeStream);
 
-		var writeStream = fs.createWriteStream('/tmp/'+value+'/'+filename);
-		part.pipe(writeStream);
+      part.on('data',function(chunk){
+        console.log(filename+' read '+chunk.length + 'bytes');
+      });
 
-		part.on('data',function(chunk){
-			console.log(filename+' read '+chunk.length + 'bytes');
-		});
-
-		part.on('end',function(){
-			console.log(filename+' Part read complete');
-			writeStream.end();
-		});
+      part.on('end',function(){
+        console.log(filename+' Part read complete');
+        writeStream.end();
+			});
+    });
 	});
-});
 
-form.parse(req);
+	// all uploads are completed
+	form.on('close',function(){
+		res.status(200).send('Upload complete');
+	});
 
-	// form.parse(req, function(err, fields, files) {
-	// 	console.log(fields);
-	// 	Object.keys(fields).forEach(function(name) {
-	// 		console.log('got field named ' + name);
-	// 		console.log('got field value ' + fields[name]);
-	// 	});
-	//
-	// 	Object.keys(files).forEach(function(name) {
-	// 		console.log('got file named ' + name);
-	// 		console.log('got file value ' + files[name]);
-	// 	});
-	//
-	// 	var tid = fields['tid'];
-	//
-	// 	// filename = files['myfile1'][0].filename;
-	//
-	// 	console.log('Upload completed!');
-	// 	//		res.setHeader('text/plain');
-	// 	res.end('Received ' + files.length + ' files');
-	// });
+	// track progress
+	form.on('progress',function(byteRead,byteExpected){
+		console.log(' Reading total  '+byteRead+'/'+byteExpected);
+	});
+
+	form.parse(req);
 
 });
 
