ab532a2ba3b352919c44fa60797563df97fd6afd
hyunjuma <flipped71@gmail.com>
2016-08-23 17:10:33 +0900

    callback ..

--- router/projects.js
+++ router/projects.js
@@ -121,6 +121,9 @@ var Project = require('../models/project');
           var path_task = (path+'/'+tid);
           console.log(tid + ' has something in it');
           console.log("safe i   " + i);
+
+          generate_callback(i);
+
           fs.readdir(path_task, function(err, items) {
             product[i] = new Array(items.length); //made a double array, looks like 'product[tasknumber][pdt];'
             product[i][0] = tid;
@@ -129,12 +132,14 @@ var Project = require('../models/project');
             for(var j=1; j<=items.length; j++) {
               product[i][j] = items[j];
               console.log(product[i][0]+'에 들어있는거: '+product[i][j]);
+              function generate_callback(param){
+                console.log("does nothing");
+                return ;
+              }
             }
           })
         }
       }
-
-      //fs.lstatSync(path_string).isDirectory()
 
 //       fs.readdir(path, function(err, items) {
 //           for (var i=0; i<items.length; i++) {
