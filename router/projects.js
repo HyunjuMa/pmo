bbdc10eecc3deb74f5aa624ebb8c35ff45abf966
hyunjuma <flipped71@gmail.com>
2016-08-28 17:22:50 +0900

    ?

--- router/projects.js
+++ router/projects.js
@@ -322,12 +322,32 @@ app.get('/download/:pid/:tid/:filename', function(req, res) {
 
   var file = fs.readFile(dir, 'utf8', function (err, data) {
     if(err) return console.log('error...' + err);
-    console.log(data);
+    //console.log(data);
   });
-  fs.writeFileSync(('/tmp/'+filename), file);
+  //fs.writeFileSync(('/tmp/'+filename), file);
 
-  res.redirect('/project/'+pid);
+  res.download(file);
+  //res.redirect('/project/'+pid);
 })
+
+
+// var path = require('path');
+// var mime = require('mime');
+//
+// app.get('/download', function(req, res){
+//
+//   var file = __dirname + '/upload-folder/dramaticpenguin.MOV';
+//
+//   var filename = path.basename(file);
+//   var mimetype = mime.lookup(file);
+//
+//   res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+//   res.setHeader('Content-type', mimetype);
+//
+//   var filestream = fs.createReadStream(file);
+//   filestream.pipe(res);
+// });
+
 
 
 
